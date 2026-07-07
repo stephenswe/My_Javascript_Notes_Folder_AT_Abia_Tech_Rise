be5c291581b23fd3592de38ca8b8d4e25e6ea1fe
stephenswe <lilcrystalhimself@gmail.com>
2026-07-07 13:57:01 +0100

    updated the quiz.js file

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -1,104 +1,114 @@
 const quizQuestions = [
     {
-        question: "What does HTML stand for?",
-        options: [
-            "High Text Markup Language",
-            "Hyper Text Markup Language",
-            "Hyper Transfer Markup Language",
-            "High Transfer Machine Language"
-        ],
-        correctIndex: 1
+      question: "What does HTML stand for?",
+      options: [
+        "High Text Markup Language",
+        "Hyper Text Markup Language",
+        "Hyper Transfer Markup Language",
+        "High Transfer Machine Language"
+      ],
+      correctIndex: 1
     },
     {
-        question: "Which method selects the FIRST matching element?",
-        options: [
-            "querySelectorAll()",
-            "getElementsByClassName()",
-            "querySelector()",
-            "getAllElements()"
-        ],
-        correctIndex: 2
+    question: "Which method selects the FIRST matching element?",
+    options: [
+      "querySelectorAll()",
+      "getElementsByClassName()",
+      "querySelector()",
+      "getAllElements()"
+    ],
+    correctIndex: 2
     },
-
     {
-        question: "Which event fires when a button is clicked?",
-        options: [
-            "\"hover\"",
-            "\"submit\"",
-            "\"click\"",
-            "\"load\""
-        ],
-        correctIndex: 2
-    },
-
+         question: "Which event fires when a button is clicked?",
+    options: [
+      "\"hover\"",
+      "\"submit\"",
+      "\"click\"",
+      "\"load\""
+    ],
+    correctIndex: 2
+    } ,
     {
-        question: "What does DOM stand for?",
-        options: [
-            "Document Object Model",
-            "Data Object Method",
-            "Display Output Mode",
-            "Direct Order Machine"
-        ],
-        correctIndex: 0
+         question: "What does DOM stand for?",
+    options: [
+      "Document Object Model",
+      "Data Object Method",
+      "Display Output Mode",
+      "Direct Order Machine"
+    ],
+    correctIndex: 0
 
     }
 ]
 
-
-//taragetting the items
-
+// Targeting the items
 const questionText = document.getElementById("question-text");
-
-const optionBtn = document.getElementsByClassName("option-btn");
-
+const optionBtn = document.querySelectorAll(".option-btn");
 const nextBtn = document.getElementById("next-btn");
-
-const  scoreText = document.getElementById("score-text");
+const scoreText = document.getElementById("score-text");
 
 let currentQuestionIndex = 0;
-
 let score = 0;
-
 let hasAnswered = false;
 
-
-// a function to the load question
-
-function loadQuestion() {
+function loadQuestion () {
     hasAnswered = false;
 
-
-    const current = quizQuestions[currentQustionIndex];
-
+    const current = quizQuestions[currentQuestionIndex];
     questionText.textContent = current.question;
 
     // using forEach to loop through the options
-
-    optionBtn.forEach(function (button, index){
-        button.textContent = current. options[index];
-        button.style.backgroundColor = "";
-    })
-
+optionBtn.forEach(function(button, index) {
+    button.textContent = current.options[index];
+    button.style.backgroundColor = "";
+});
 }
 
-// this is a function for checking your answer
-function  checkAnswer (selectedIndex){
-    if (hasAnswered){
-        return;
-    }
 
-    hasAnswered = true;
-    const current = quizQuestions [currentQuestionIndex];
-    const correctIndex = current.correctIndex;
+//This function is for checking your answer
+function checkAnswer (selectedIndex) {
+  if (hasAnswered) {
+    return;
+  }
+  hasAnswered = true; 
+  const current = quizQuestions
+  [currentQuestionIndex];
+  const correctIndex = current.correctIndex;
 
-
-    if(selectedIndex === correctIndex){
-        optionButtons[selectedIndex].style.backgroundColor = "green";
-        score = score + 1;
-
-    }else {
-        optionButtons[selectedIndex].style.backgroundColor = "salmon";
-        optionButtons =[correctIndex].style.backgroundColor = "green";
-    }
-
+  if (selectedIndex === correctIndex) {
+    optionBtn[selectedIndex].style.backgroundColor = "green";
+    score = score + 1;
+  } else {
+    optionBtn[selectedIndex].style.backgroundColor = "red";
+    optionBtn[correctIndex].style.backgroundColor = "green";
+  }
+  nextBtn.style.display = "block";
 }
+
+// Moving to the next question
+function nextQuestion() {
+  currentQuestionIndex = currentQuestionIndex + 1;
+
+  if (currentQuestionIndex < quizQuestions.length) {
+    loadQuestion();
+    nextBtn.style.display = "none";
+  } else {
+    document.getElementById("question-text").style.display = "none";
+  optionBtn.forEach (function (button) {
+    button.style.display = "none";
+  });
+  nextBtn.style.display = "none";
+  scoreText.style.display = "block";
+  scoreText.textContent = "You scored " + score + "out of" + quizQuestions.length; 
+  }
+}
+
+optionBtn.forEach(function (button, index) {
+  button.addEventListener("click", function() {
+    checkAnswer(index);
+  });
+})
+
+nextBtn.addEventListener("click", nextQuestion);
+loadQuestion();
